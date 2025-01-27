efffeedc8ba53412972b2d047e4b9e0842c9e8a5
Inoo5246 <52535085+Inoo5246@users.noreply.github.com>
2025-01-27 15:12:11 +0200

    Update dynu.sh

--- dynu.sh
+++ dynu.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# Înlocuiește USERNAME și PASSWORD cu datele tale
+# Folosim variabilele de mediu setate în docker-compose.yml
 USERNAME=${DYNU_USERNAME}
 PASSWORD=${DYNU_PASSWORD}
 
-# Actualizare DNS
+# Verificăm dacă variabilele de mediu sunt setate
+if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
+  echo "Error: Username or Password not set!"
+  exit 1
+fi
+
+# Actualizare DNS Dynu
 echo "Updating Dynu DNS..."
 curl -k "https://api.dynu.com/nic/update?username=${USERNAME}&password=${PASSWORD}" -o /usr/local/bin/dynu.log
